5d4e203e471717a463891506d3337b2231269b67
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-22 07:24:36 +0000

    Deploy RomanBezpalyi/js-band-store-hooks to github.com/RomanBezpalyi/js-band-store-hooks.git:gh-pages

---
+++ precache-manifest.4811000efb66eee47deda27b52cbb871.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95acbe9d3adb06a88b1d4dfea1df9f9a",
+    "url": "/js-band-final-task/index.html"
+  },
+  {
+    "revision": "7f029a3e70178f8f1dda",
+    "url": "/js-band-final-task/static/css/2.1b28e48e.chunk.css"
+  },
+  {
+    "revision": "b39fee8cfa57a90e85a8",
+    "url": "/js-band-final-task/static/css/main.3c21ffd4.chunk.css"
+  },
+  {
+    "revision": "7f029a3e70178f8f1dda",
+    "url": "/js-band-final-task/static/js/2.e6f730d5.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/js-band-final-task/static/js/2.e6f730d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b39fee8cfa57a90e85a8",
+    "url": "/js-band-final-task/static/js/main.6cc277a4.chunk.js"
+  },
+  {
+    "revision": "328f44d80c29b594f89e",
+    "url": "/js-band-final-task/static/js/runtime-main.4fc669c0.js"
+  },
+  {
+    "revision": "0fdbfd87354b6ce7caeb922f25bf94b4",
+    "url": "/js-band-final-task/static/media/JS_BAND_STORE.0fdbfd87.png"
+  },
+  {
+    "revision": "a8ece28009baedd481ae770f946e8308",
+    "url": "/js-band-final-task/static/media/shopping-cart.a8ece280.svg"
+  }
+]);
